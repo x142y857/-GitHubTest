f72cd458d0b8bf7123fb603b4762df52f6f0eb73
翁子皓 <x142y857@gmail.com>
2021-06-22 09:43:55 +0800

    5-2

--- main.kt
+++ main.kt
@@ -1,6 +1,6 @@
 fun main(){
-    println({
+    val greetingFunction: () -> String ={
         val currentYear = 2018
         "歡迎來到 SimVillage, 翁子皓! (copyright $currentYear)"
-    }())
-}
+        println(greetingFunction())
+    }

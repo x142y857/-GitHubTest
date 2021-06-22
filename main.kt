88a7561edb572ab3de8e354e9b6ba69bef668d76
翁子皓 <x142y857@gmail.com>
2021-06-22 10:19:26 +0800

    5-7

--- main.kt
+++ main.kt
@@ -1,8 +1,13 @@
 fun main(){
-    val greetingFunction = {playerName: String,numBuildings: Int->
+    val greetingFunction = { playerName: String,numBuildings: Int->
         val currentYear = 2018
         println("Adding $numBuildings houses")
         "歡迎來到 SimVillage, $playerName! (copyright $currentYear)"
     }
-    println(greetingFunction("翁子皓",2))
+    runSimulation("翁子皓",greetingFunction)
+}
+fun runSimulation(playerName: String,greetingFunction: (String,Int)->String )
+{
+    val numBuildings = (1..3).shuffled().last() //Randomly selects 1,2, or 3
+    println(greetingFunction(playerName,numBuildings))
 }

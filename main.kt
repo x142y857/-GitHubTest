32df8ac919d1e71728d66aa56617a6baaaafc7b3
翁子皓 <x142y857@gmail.com>
2021-06-22 10:03:54 +0800

    5-6

--- main.kt
+++ main.kt
@@ -1,5 +1,5 @@
 fun main(){
-    val greetingFunction: (String,Int) -> String ={playerName,numBuildings->
+    val greetingFunction = {playerName: String,numBuildings: Int->
         val currentYear = 2018
         println("Adding $numBuildings houses")
         "歡迎來到 SimVillage, $playerName! (copyright $currentYear)"

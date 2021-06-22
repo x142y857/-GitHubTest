47894ea61aeeb60f48a079c4004ec7d40dd08dc4
翁子皓 <x142y857@gmail.com>
2021-06-22 10:57:56 +0800

    5-13

--- main.kt
+++ main.kt
@@ -1,17 +1,18 @@
 fun main(){
-    runSimulation("翁子皓", ::printConstructionCost){playerName,numBuildings->
-        val currentYear = 2018
-        println("添加 $numBuildings 房子")
-        "歡迎來到 SimVillage, $playerName! (copyright $currentYear)"
-    }
-}
-inline fun runSimulation(playerName: String,cosPrinter: (Int)->Unit,greetingFunction: (String,Int)->String ) {
-    val numBuildings = (1..3).shuffled().last() //Randomly selects 1,2, or 3
-    cosPrinter(numBuildings)
-    println(greetingFunction(playerName,numBuildings))
+    runSimulation()
 }
 
-fun printConstructionCost(numBuildings: Int){
-    val cost = 500
-    println("建築成本: ${cost * numBuildings}")
+fun runSimulation(){
+    val greetingFunction = configureetingFunction()
+    println(greetingFunction("翁子皓"))
+}
+fun configureetingFunction():((String)->String ) {
+    val structureType = "醫院"
+    var numBuildings = 5
+    return { playerName: String ->
+        val currentYear = 2018
+        numBuildings +=1
+        println("添加 $numBuildings $structureType")
+        "歡迎來到 SimVillage, $playerName! (copyright $currentYear)"
+    }
 }

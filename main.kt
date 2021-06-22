aa1d2843cbcec7ba6398c7079b81ed87fc08da9e
翁子皓 <x142y857@gmail.com>
2021-06-22 10:40:19 +0800

    5-11

--- main.kt
+++ main.kt
@@ -5,9 +5,9 @@ fun main(){
         "歡迎來到 SimVillage, $playerName! (copyright $currentYear)"
     }
 }
-inline fun runSimulation(playerName: String,greetingFunction: (String,Int)->String )
-{
+inline fun runSimulation(playerName: String,cosPrinter: (Int)->Unit,greetingFunction: (String,Int)->String ) {
     val numBuildings = (1..3).shuffled().last() //Randomly selects 1,2, or 3
+    cosPrinter(numBuildings)
     println(greetingFunction(playerName,numBuildings))
 }
 
